614cd69b7c926a0ebc62849fa142357dd6d0eea2
Cody Yu <comaniac0422@gmail.com>
2021-11-04 13:35:19 -0700

    [Module] Import versions (#19)

--- torch_mnm/__init__.py
+++ torch_mnm/__init__.py
@@ -2,7 +2,7 @@
 Note that the import order in this file cannot be changed,
 because the order of loading shared libraries matters.
 """
-# pylint: disable=wrong-import-order
+# pylint: disable=wrong-import-order, useless-import-alias
 from . import _lib
 
 import torch
@@ -13,3 +13,12 @@ import lazy_tensor_core
 from . import jit
 from . import torch_parameter
 from . import optimizer
+
+try:
+    from .version import __version__ as __version__
+    from .version import __mnm_gitrev__ as __mnm_gitrev__
+    from .version import __torch_gitrev__ as __torch_gitrev__
+except: # pylint: disable=bare-except
+    __version__ = "dev"
+    __mnm_gitrev__ = "unknown"
+    __torch_gitrev__ = "unkonwn"
