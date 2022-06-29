63ff71cdfd6e26dc01f47d8da983f138b97d4a16
Cody Yu <comaniac0422@gmail.com>
2022-06-29 13:02:27 -0700

    [CI] Add nightly CI (#10)

--- ci/batch/task_python_distributed_pt_1_11.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-set -e
-set -u
-set -o pipefail
-
-LTC_IO_THREAD_POOL_SIZE=1 mpirun -np 2 --allow-run-as-root python3 tests/python/op/test_distributed.py -m "torch_1_11_test and torch_all_test"
