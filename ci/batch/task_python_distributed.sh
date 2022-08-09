f192ac59b9094540dd9946bc4cee4533de8d49d4
Shawnak Deb <shawnakdeb@yahoo.com>
2022-08-09 23:03:36 +0000

    Added test_ratex_fully_sharded_data_parallel to the automated testing suite

--- ci/batch/task_python_distributed.sh
+++ ci/batch/task_python_distributed.sh
@@ -15,3 +15,4 @@ NUM_WORKERS=$RATEX_DEVICE_COUNT
 
 LTC_IO_THREAD_POOL_SIZE=1 mpirun -np $NUM_WORKERS --allow-run-as-root python3 tests/python/op/test_distributed.py
 LTC_IO_THREAD_POOL_SIZE=1 mpirun -np $NUM_WORKERS --allow-run-as-root python3 tests/python/model/test_distributed_models.py
+LTC_IO_THREAD_POOL_SIZE=1 mpirun -np $NUM_WORKERS --allow-run-as-root python3 tests/python/model/test_ratex_fully_sharded_data_parallel.py
