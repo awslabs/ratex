b2830b21580dbc9895a8f8d882783fec8b42ceac
Shawnak Deb <shawnakdeb@yahoo.com>
2022-08-15 14:59:19 +0000

    Fixed padding dimension to be first dimension instead of last dimension

--- ratex/core/distributed/ratex_fully_sharded_data_parallel.py
+++ ratex/core/distributed/ratex_fully_sharded_data_parallel.py
@@ -63,9 +63,11 @@ class RatexFullyShardedDataParallel(nn.Module):
         Returns:
           A tensor that corresponds to the respective shard of this rank
         """
-        if tensor.numel() % self.world_size != 0:
-            pad_size = self.world_size - (tensor.numel() % self.world_size)
-            tensor = F.pad(tensor, (0, pad_size))
+
+        if tensor.size()[0] % self.world_size != 0:
+            padding = [0] * (len(tensor.size()) * 2)
+            padding[-1] = self.world_size - (tensor.size()[0] % self.world_size)
+            tensor = F.pad(tensor, tuple(padding))
         tensor = tensor.chunk(self.world_size)[self.rank]
         return tensor
 
@@ -107,6 +109,6 @@ class RatexFullyShardedDataParallel(nn.Module):
 
         # All gather the new weights across the ranks and assign them to the full parameters
         for param, shard in zip(self.params, self.sharded_params):
-            all_gather(shard.data, dim=0, output=param.data)
+            param.data = all_gather(shard.data, dim=0)[: param.data.shape[0]]
 
         return loss
